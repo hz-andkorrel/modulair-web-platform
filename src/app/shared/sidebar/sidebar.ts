f60cf253c4ace2000c86059af72b55543aff50d4
Julien Nout <nout0001@hz.nl>
2025-10-02 13:24:30 +0200

    added angular materials library

---
+++ src/app/shared/sidebar/sidebar.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule, 
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
+  ],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.scss'
+})
+export class SidebarComponent {
+  
+}
