1eb17080425edd42a7c85e9ffa3723a08cd6a01d
Jens Joosse <joos0067@hz.nl>
2025-11-10 12:25:50 +0100

    eerste instellingen + balken

--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -7,5 +7,6 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SettingsComponent {
-  
+ templateurl = `./settings.component.html`;
+ styleurl = `./settings.component.scss`;
 }
