6cd1053306026529c060bb8d9e58610595417e5f
Julien Nout <nout0001@hz.nl>
2025-12-08 09:27:26 +0100

    midway commit

--- src/app/features/plugins/plugin-mews.component.ts
+++ src/app/features/plugins/plugin-mews.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-plugin-mews',
+  imports: [MatDivider],
   template: `
+    <mat-divider />
     <iframe 
       src="http://localhost:8080/render"
       style="width: 100%; height: calc(100vh - 1px); border: none;">
