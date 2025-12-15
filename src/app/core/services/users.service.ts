4ac65d01a84acc10dde215247ab2e6bd8d85f619
Lars Sonke <lars.sonke1@gmail.com>
2025-12-15 09:09:20 +0100

    feat: add first-time setup component and users service

---
+++ src/app/core/services/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+export interface MeResponse {
+  id: string;
+  email: string;
+  display_name?: string;
+  first_time_setup?: boolean;
+  language?: string;
+  timezone?: string;
+  organization?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class UsersService {
+  private readonly API = `${environment.apiUrl}/users`;
+
+  constructor(private http: HttpClient) {}
+
+  getMe(): Observable<MeResponse> {
+    return this.http.get<MeResponse>(`${this.API}/me`);
+  }
+
+  updateMe(payload: Partial<MeResponse>) {
+    return this.http.patch<MeResponse>(`${this.API}/me`, payload);
+  }
+}
