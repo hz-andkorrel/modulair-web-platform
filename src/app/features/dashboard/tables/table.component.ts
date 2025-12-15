048e1c1f5225863760280722561f2fa42c579c42
Julien Nout <nout0001@hz.nl>
2025-12-15 14:40:23 +0100

    minor contents fix

--- src/app/features/dashboard/tables/table.component.ts
+++ src/app/features/dashboard/tables/table.component.ts
@@ -27,7 +27,11 @@ export class TableComponent {
   dataSource = [
     { column1: 'Data 1', column2: 'Data 2', column3: 'Data 3' },
     { column1: 'Data 4', column2: 'Data 5', column3: 'Data 6' },
-    { column1: 'Data 7', column2: 'Data 8', column3: 'Data 9' }
+    { column1: 'Data 7', column2: 'Data 8', column3: 'Data 9' },
+    { column1: 'Data 7', column2: 'Data 8', column3: 'Data 9' },
+    { column1: 'Data 7', column2: 'Data 8', column3: 'Data 9' },
+    { column1: 'Data 7', column2: 'Data 8', column3: 'Data 9' },
+    { column1: 'Data 7', column2: 'Data 8', column3: 'Data 9' },
   ];
 
   onClose() {
