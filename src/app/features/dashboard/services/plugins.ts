4ac895a41a150830cbaafe5ef5829c17b2da3708
Julien Nout <nout0001@hz.nl>
2025-12-15 11:53:46 +0100

    import plugin dummy data for widgets

---
+++ src/app/features/dashboard/services/plugins.ts
@@ -0,0 +1,53 @@
+import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PluginDef } from '../widgets/widgets';
+import { plugins as registryPlugins, Plugin } from '../../shared/registry-data';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PluginsService {
+  plugins = signal<PluginDef[]>([]);
+  loading = signal(false);
+  error = signal<string | null>(null);
+
+  constructor(private http: HttpClient) {
+    this.loadPlugins();
+  }
+
+  loadPlugins() {
+    this.loading.set(true);
+    this.error.set(null);
+    
+    // TODO: Replace with actual API endpoint
+    // this.http.get<Plugin[]>('/api/plugins').subscribe({
+    //   next: (data) => {
+    //     this.plugins.set(this.transformPlugins(data));
+    //     this.loading.set(false);
+    //   },
+    //   error: (err) => {
+    //     this.error.set(err.message);
+    //     this.loading.set(false);
+    //   }
+    // });
+
+    // Load from registry data and transform
+    const transformed = this.transformPlugins(registryPlugins());
+    this.plugins.set(transformed);
+    this.loading.set(false);
+  }
+
+  private transformPlugins(registryPlugins: Plugin[]): PluginDef[] {
+    return registryPlugins.map(plugin => ({
+      id: plugin.id,
+      name: plugin.name,
+      widgets: [
+        {
+          id: plugin.id,
+          title: plugin.name,
+          description: plugin.description
+        }
+      ]
+    }));
+  }
+}
